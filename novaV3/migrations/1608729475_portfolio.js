aa1e1c3d42d43321142409a6b998fa66c9f2806f
= <=>
2021-07-13 09:55:42 +0100

    add test unitaire

--- novaV3/migrations/1608729475_portfolio.js
+++ novaV3/migrations/1608729475_portfolio.js
@@ -1,8 +1,17 @@
-const Portfolio = artifacts.require("PortfolioManagement");
+const PortfolioNFT = artifacts.require("PortfolioNFT");
+const PortfolioDataObjects = artifacts.require("PortfolioDataObjects");
+const PortfolioDataStorages = artifacts.require("PortfolioDataStorages");
+const PortfolioData = artifacts.require("PortfolioData");
+const PortfolioManagement = artifacts.require("PortfolioManagement");
 
 module.exports = async function(_deployer) {
    
-   await _deployer.deploy(Portfolio);
+  // await _deployer.deploy(PortfolioNFT);
+   await _deployer.deploy(PortfolioDataObjects);
+   await _deployer.deploy(PortfolioDataStorages);
+   await _deployer.deploy(PortfolioData);
+   let portfolioData = await PortfolioData.deployed();
+   await _deployer.deploy(PortfolioManagement, portfolioData.address);
  
   
 };
